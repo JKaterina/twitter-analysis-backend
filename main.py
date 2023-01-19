651af79c4bb2c2c4cbee0ce96e182beb5a4671a8
JKaterina <uljanova.jekaterina@gmail.com>
2023-01-19 22:16:01 +0100

    Implemented process_tweet_data

--- main.py
+++ main.py
@@ -21,7 +21,20 @@ def extract_tweet_data(api, twitter_handle, timeframe):
                            count=2,
                            include_rts = True,
                            tweet_mode = 'extended')
+    #To do: implement engagement api to extract quote tweets, replies, video views, url link clicks, user profile clicks, engagements, impressions
     return tweets
+
+def process_tweet_data(tweets_json):
+    #To add: quote tweets, replies, video views, url link clicks, user profile clicks, engagements, impressions
+    
+    outtweets = [[tweet["id_str"],
+              tweet["created_at"],
+              tweet["retweet_count"],
+              tweet["favorite_count"]] 
+             for idx, tweet in enumerate(tweets_json)]
+    df = pd.DataFrame(outtweets,columns=["id","created_at","retweet_count","favorite_count"])
+    df.to_csv('processed_data/%s_tweets.csv' % twitter_handle,index=False)
+    return True
 
 if __name__ == "__main__":
     api = authenticate()
@@ -30,8 +43,14 @@ if __name__ == "__main__":
     tweets_list_of_dicts = []
     for tweet in tweets:
         tweets_list_of_dicts.append(tweet._json)
+    if tweets: print("Tweets extracted")
     with open('raw_data/tweets.json', 'w') as file:
-        file.write(json.dumps(tweets_list_of_dicts, indent=4))
+        json_string = json.dumps(tweets_list_of_dicts, indent=4)
+        file.write(json_string)  
+        data = json.loads(json_string)
+        process_tweet_data(data)
+        print("Tweets processed")
+        
 
     
 
